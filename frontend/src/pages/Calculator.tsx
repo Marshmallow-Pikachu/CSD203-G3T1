cde07bb6f0a444cb8edfaafef81d1c22ab7dcbcb
Marshmallow Pikachu <marshmallowpikachu@gmail.com>
2025-09-25 16:06:46 +0800

    Tweaked the calculator fields

--- frontend/src/pages/Calculator.tsx
+++ frontend/src/pages/Calculator.tsx
@@ -9,15 +9,20 @@ export default function Calculator(){
                 <div className="container-row">
                     <div className="child">
                         <InputField label="Product Description" name="product_description" type="" />
-                        <InputField label="Country of Origin" name="country_of_origin" type="" />
+                        <InputField label="Country of Origin" name="" type="" />
                         <InputField label="Product Value (USD)" name="product_value" type="number" />
-                        <InputField label="Shipping Cost (USD)" name="shipping_cost" type="number" />
+                        <InputField label="Agreement" name="agreement" type="number" />
+                        <InputField label="Product Quantity" name="product_quantity" type="number" />
                     </div>
                     <div className="child">
                         <InputField label="HS Code" name="hs_code" type="number" />
                         <InputField label="Destination Country" name="destination_country" type="" />
-                        <InputField label="Product Quantity" name="product_quantity" type="number" />
+                        <InputField label="Shipping Cost (USD)" name="shipping_cost" type="number" />
                         <InputField label="Insurance" name="insurance" type="number" />
+                        <div className="container-row">
+                            <InputField label="Start Date" name="start_date" type="date"/>
+                            <InputField label="End Date" name="end_date" type="date" />
+                        </div>
                     </div>
                 </div>
                 <button type="submit">Calculate Landed Cost</button>
